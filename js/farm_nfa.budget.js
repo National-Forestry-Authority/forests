6bf9b577ea373b46d093f410f071f64553317fd8
Michael Stenta <mike@mstenta.net>
2019-08-12 15:20:19 -0400

    Calculate the budget grand total and display it at the bottom.

--- js/farm_nfa.budget.js
+++ js/farm_nfa.budget.js
@@ -12,6 +12,10 @@
         Drupal.behaviors.farm_nfa_forest_budget.calculateRowTotal(row);
         Drupal.behaviors.farm_nfa_forest_budget.calculateGrandTotal();
       });
+      $('#' + form_id + ' .total', context).on('keyup', function() {
+        Drupal.behaviors.farm_nfa_forest_budget.calculateGrandTotal();
+      });
+      this.calculateGrandTotal();
     },
     calculateRowTotal: function(row) {
       var cost = $('.cost', row).val();
@@ -19,6 +23,22 @@
       var total = cost * qty;
       if (total === total) {
         $('.total', row).val(total);
+      }
+    },
+    calculateGrandTotal: function() {
+      var form_id = 'farm-nfa-forest-plan-budget-form';
+      var total = 0;
+      $('#' + form_id + ' table tr').each(function() {
+        var rowTotal = parseFloat($(this).find('input.total').val());
+        if (!isNaN(rowTotal)) {
+          total += rowTotal;
+        }
+      });
+      if (!isNaN(total)) {
+        $('#budget-total').html('<strong>Budget Total</strong>: ' + total);
+      }
+      else {
+        $('#budget-total').html('');
       }
     }
   };
