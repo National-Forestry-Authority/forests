e9f70c699c3a0ff5643418466095f258ed9b3edd
Pedro Cambra <pedro@cambra.io>
2021-10-05 19:32:40 +0200

    #102 Added natural inventory log type

---
+++ web/modules/custom/farm_nfa/modules/farm_nfa_natural_forest/src/Plugin/Log/LogType/NaturalInventory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\farm_nfa_natural_forest\Plugin\Log\LogType;
+
+use Drupal\farm_entity\Plugin\Log\LogType\FarmLogType;
+
+/**
+ * Provides the natural inventory log type.
+ *
+ * @LogType(
+ *   id = "natural_inventory",
+ *   label = @Translation("Natural inventory"),
+ * )
+ */
+class NaturalInventory extends FarmLogType {
+
+}
