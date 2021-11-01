51b4c6ce365fb0e64a08af3596b3731b77ea6d06
Pedro Cambra <pedro@cambra.io>
2021-11-01 12:39:44 +0100

    #92 Added seedlings and changed the enrichment label

--- web/modules/custom/farm_nfa/modules/farm_nfa_planting/src/Form/ForestPlanPlantingForm.php
+++ web/modules/custom/farm_nfa/modules/farm_nfa_planting/src/Form/ForestPlanPlantingForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\farm_nfa_planting\Form;
 
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\farm_nfa\Form\ForestPlanBaseForm;
 
 /**
@@ -26,6 +27,35 @@ class ForestPlanPlantingForm extends ForestPlanBaseForm {
         'log_type' => 'planting',
         'form_title' => t('Planting'),
       ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildForm($form, $form_state);
+
+    /** @var \Drupal\log\Entity\LogInterface $log */
+    $log = $form['#log'];
+    if (!$log->isNew() && isset($form['quantity'])) {
+      foreach ($log->get('quantity')->referencedEntities() as $quantity) {
+        if ($quantity->get('units')->entity->get('name')->value == 'trees') {
+          $total_trees = $quantity->get('value')->value;
+        }
+        if ($quantity->get('units')->entity->get('name')->value == 'hectares') {
+          $hectares = $quantity->get('value')->value;
+        }
+      }
+      if ($total_trees && $hectares) {
+        $form['seedlings_per_hectare'] = [
+          '#type' => 'markup',
+          '#markup' => $this->t('Number of seedlings per hectares: %value', ['%value' => round($total_trees / $hectares, 2)]),
+          '#weight' => $form['quantity']['#weight'] + 1,
+        ];
+      }
+    }
+
+    return $form;
   }
 
 }
