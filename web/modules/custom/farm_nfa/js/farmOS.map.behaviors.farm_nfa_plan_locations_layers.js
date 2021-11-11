cb6d5e0abcd9dd688e25cec504805d4efc1cadb2
Pedro Cambra <pedro@cambra.io>
2021-11-11 13:07:48 +0100

    #130 Add the popup behaviour to the plan summary dashboard map

--- web/modules/custom/farm_nfa/js/farmOS.map.behaviors.farm_nfa_plan_locations_layers.js
+++ web/modules/custom/farm_nfa/js/farmOS.map.behaviors.farm_nfa_plan_locations_layers.js
@@ -11,6 +11,24 @@
       source.on('change', function () {
         instance.zoomToVectors()
       })
+
+      // Load area details via AJAX when an area popup is displayed.
+      instance.popup.on('farmOS-map.popup', function (event) {
+        var link = event.target.element.querySelector('.ol-popup-name a');
+        if (link) {
+          var assetLink = link.getAttribute('href')
+          var description = event.target.element.querySelector('.ol-popup-description');
+          description.innerHTML = 'Loading asset details...';
+          fetch(assetLink + '/map-popup')
+            .then((response) => {
+              return response.text();
+            })
+            .then((html) => {
+              description.innerHTML = html;
+              instance.popup.panIntoView();
+            });
+        }
+      })
     }
   }
 }())
