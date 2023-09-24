dec319b8333341423391c2c28a5fa819d8647fa3
yash621 <yg17381@gmail.com>
2023-09-25 03:24:04 +0530

    #275: added layer switcher autoopening

---
+++ web/modules/custom/farm_nfa/js/farmOS.map.behaviors.farm_nfa_layerswitcher_sidebar.js
@@ -0,0 +1,16 @@
+(function ($, Drupal) {
+  farmOS.map.behaviors.farm_nfa_layerswitcher_sidebar = {
+    attach: function (instance) {
+      // opening layer switcher side bar by default
+      window.onload = () => {
+        const sideBar = document.querySelectorAll('button[role="tab"]');
+        console.log(sideBar)
+        if (sideBar.length > 0) {
+          sideBar.forEach((element) => {
+            element.click();
+          });
+        }
+      };
+    }
+  }
+}())
