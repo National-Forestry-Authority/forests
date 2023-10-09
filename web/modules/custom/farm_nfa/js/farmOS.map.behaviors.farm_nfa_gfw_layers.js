7f20b40bfc5c7f1adf9fc180ed7ce72456f51df7
yash621 <yg17381@gmail.com>
2023-10-09 20:05:17 +0530

    #273: reverted console statements

--- web/modules/custom/farm_nfa/js/farmOS.map.behaviors.farm_nfa_gfw_layers.js
+++ web/modules/custom/farm_nfa/js/farmOS.map.behaviors.farm_nfa_gfw_layers.js
@@ -148,7 +148,6 @@ async function farmNfaPlotGfwApiMap(instance, mapType, gfwApiUrl, dateRange) {
         let allLayersControllers = document.querySelectorAll(".layer-switcher input");
         allLayersControllers.forEach((layerController) => {
           const shouldDisableLayer = layerController.nextSibling.innerText !== "Fire Alerts" && layerController.nextSibling.innerText !== "Locations";
-          console.log('test')
           if (shouldDisableLayer) {
             layerController.click();
           }
