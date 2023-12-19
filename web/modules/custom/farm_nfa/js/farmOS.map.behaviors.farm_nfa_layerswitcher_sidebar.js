fa9072d8c5a4a708edaba6d5fa221b567c5c252b
yash621 <yg17381@gmail.com>
2023-12-19 11:16:00 +0530

    #288: added cfr plottings

--- web/modules/custom/farm_nfa/js/farmOS.map.behaviors.farm_nfa_layerswitcher_sidebar.js
+++ web/modules/custom/farm_nfa/js/farmOS.map.behaviors.farm_nfa_layerswitcher_sidebar.js
@@ -2,6 +2,28 @@
   farmOS.map.behaviors.farm_nfa_layerswitcher_sidebar = {
     attach: function (instance) {
       // opening layer switcher side bar by default
+      console.log(instance)
+      console.log("test")
+      console.log()
+      let assetId = window.location.href.split('/').pop();
+      if (assetId == "gfw") {
+        assetId = window.location.href.split('/').slice(-2)[0];
+      } 
+      fetch(`http://forests.ddev.site/asset/${assetId}/geojson/children`)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          const sectorId = data.features[0].properties.id
+          console.log(sectorId)
+          instance.addLayer('geojson', {
+            title: 'CFR',
+            url: `http://forests.ddev.site/asset/${sectorId}/geojson/children`,
+            color: 'green',
+          })
+        });
+      console.log(assetId)
       window.onload = () => {
         const sideBar = document.querySelectorAll('button[role="tab"]');
         if (sideBar.length > 0) {
