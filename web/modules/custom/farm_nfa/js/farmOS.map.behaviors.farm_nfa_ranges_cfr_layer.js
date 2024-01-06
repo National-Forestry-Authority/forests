731021b3fa52e510c049893aad843065c75ab282
yash621 <yg17381@gmail.com>
2024-01-07 04:13:11 +0530

    #288: added CFR layer

---
+++ web/modules/custom/farm_nfa/js/farmOS.map.behaviors.farm_nfa_ranges_cfr_layer.js
@@ -0,0 +1,37 @@
+(function ($, Drupal) {
+  farmOS.map.behaviors.farm_nfa_ranges_cfr_layer = {
+    attach: async function (instance) {
+      const assetType = document.querySelector('.field--name-name')
+      const landType = instance?.farmMapSettings?.land_type;
+      if (!(assetType && assetType.innerText.includes('Range')) && landType != 'range') { 
+        return;
+      }
+      let assetId = window.location.href.split('/').pop();
+      if (assetId == "gfw") {
+        assetId = window.location.href.split('/').slice(-2)[0];
+      }
+      try {
+        const geometryUrl = `http://forests.ddev.site/asset/${assetId}/geojson/children`
+        let geometryData = await (await fetch(geometryUrl)).json();
+        let geoJson = {
+          "type": "FeatureCollection",
+          "features": []
+        };
+        for (const feature of geometryData?.features) {
+          const sectorId = feature.properties.id
+          const url = `http://forests.ddev.site/asset/${sectorId}/geojson/children`
+          let geometry = await (await fetch(url)).json();
+          geometry?.features.forEach((feature) => {
+            geoJson.features.push(feature)
+          })
+        }
+        instance.addLayer('geojson', {
+          title: 'CFR',
+          geojson: geoJson,
+          color: 'green',
+        })
+      } catch (e) {}
+    }
+  }
+}())
+
