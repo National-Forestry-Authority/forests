c98d027faf60b87edda1abef1107c7fcff650c3f
Pedro Cambra <pedro@cambrico.net>
2024-03-21 21:14:28 +0100

    Issue #299: Storage for sub activity field

--- web/modules/custom/farm_nfa/modules/farm_nfa_cfr/src/Form/ForestCfrManagementForm.php
+++ web/modules/custom/farm_nfa/modules/farm_nfa_cfr/src/Form/ForestCfrManagementForm.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\farm_nfa_cfr\Form;
 
-use Drupal\Core\Form\FormStateInterface;
-
 /**
  * Forest CFR management form.
  *
@@ -26,70 +24,6 @@ class ForestCfrManagementForm extends ForestCfrBaseForm {
       'log_type' => 'activity',
       'form_title' => t('Management'),
     ] + parent::defaultSettings();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function buildForm(array $form, FormStateInterface $form_state) {
-    $form = parent::buildForm($form, $form_state);
-
-    // @todo Replace this with a the program/activity custom field widget.
-    $form['cfr']['#ajax'] = [
-      'wrapper' => 'activity-select-wrapper',
-      'callback' => '::activityOptions',
-      'event' => 'autocompleteclose change',
-    ];
-    $form['activity'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Program / Activity'),
-      '#weight' => 1,
-      '#prefix' => '<div id="activity-select-wrapper">',
-      '#suffix' => '</div>',
-    ];
-
-    $form['subactivity'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Sub-activity'),
-      '#weight' => 1,
-      '#prefix' => '<div id="subactivity-select-wrapper">',
-      '#suffix' => '</div>',
-    ];
-    return $form;
-  }
-
-  /**
-   * AJAX callback for loading activity summaries.
-   *
-   * @return array
-   *   The select element to replace.
-   */
-  public function activityOptions(array &$form, FormStateInterface $form_state) {
-    $selected_cfr = $form_state['values']['cfr'];
-
-    // @todo Replace this with actual data retrieval logic.
-    $activities = [1 => 'Activity 1', 2 => 'Activity 2'];
-
-    // Populate the child field options.
-    $form['activity']['#options'] = $activities;
-
-    // Return the updated child field.
-    return $form['activity'];
-  }
-
-  /**
-   * AJAX callback for loading activity summaries.
-   *
-   * @return array
-   *   The select element to replace.
-   */
-  public function subactivityOptions(array &$form, FormStateInterface $form_state) {
-    return [
-      0 => $this->t('- None -'),
-      1 => $this->t('One'),
-      2 => $this->t('Two'),
-      3 => $this->t('Three'),
-    ];
   }
 
 }
