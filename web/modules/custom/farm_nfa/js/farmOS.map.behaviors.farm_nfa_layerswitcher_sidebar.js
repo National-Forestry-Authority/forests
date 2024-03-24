b6cc58627ee69e2440d9f9943a35fd666310b6ef
yash621 <yg17381@gmail.com>
2024-03-25 01:12:03 +0530

    #318: set default layer to mapbox satellite

--- web/modules/custom/farm_nfa/js/farmOS.map.behaviors.farm_nfa_layerswitcher_sidebar.js
+++ web/modules/custom/farm_nfa/js/farmOS.map.behaviors.farm_nfa_layerswitcher_sidebar.js
@@ -3,13 +3,21 @@
     attach: function (instance) {
       // opening layer switcher side bar by default
       window.onload = () => {
-        const sideBar = document.querySelectorAll('button[role="tab"]');
-        if (sideBar.length > 0) {
-          sideBar.forEach((element) => {
-            element.click();
-          });
+        const sideBar = document.querySelector('button[role="tab"]');
+        if (sideBar) {
+          sideBar.click();
+          this.setDefaultBaseLayer();
         }
       };
+    },
+    setDefaultBaseLayer: function () {
+      const baseLayerOptions = document.querySelectorAll('.layer-switcher-base-group .layer');
+      baseLayerOptions?.forEach((layer) => {
+        if (layer.innerText.includes('Mapbox Satellite')) {
+          const radioButton = layer.querySelector('input[type="radio"]');
+          radioButton.click();
+        }
+      });
     }
   }
 }())
