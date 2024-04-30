35bf5f72cb7275aabe4c3b0bf98951c242bda837
miriam <miriam@cambrico.net>
2024-04-30 11:03:55 +0200

    Issue #323: cleanup old update hooks

--- web/modules/custom/farm_nfa/farm_nfa.post_update.php
+++ web/modules/custom/farm_nfa/farm_nfa.post_update.php
@@ -5,64 +5,12 @@
  * Post update functions for the farm_nfa module.
  */
 
-use Drupal\taxonomy\Entity\Term;
-
 /**
- * Save the taxonomy terms for forest_vegetation taxonomy type.
+ * Implements hook_removed_post_updates().
  */
-function farm_nfa_post_update_001_add_forest_vegetation_terms(&$sandbox) {
-  $vocab_terms = [
-    'forest_vegetation' => [
-      'Tropical High Forest',
-      'Woodland',
-      'Bush',
-    ],
+function farm_nfa_removed_post_updates() {
+  return [
+    'farm_nfa_post_update_001_add_forest_vegetation_terms' => '10.0.0',
+    'farm_nfa_post_update_002_add_forest_purpose_terms' => '10.0.0',
   ];
-  foreach ($vocab_terms as $vocab => $terms) {
-    $current_vocab = $vocab;
-    // Term names to be added.
-    foreach ($terms as $term) {
-      $term = Term::create([
-        'name' => $term,
-        'vid' => $current_vocab,
-      ]);
-      $term->save();
-    }
-  }
-}
-
-/**
- * Save the taxonomy terms for forest purpose taxonomy type.
- */
-function farm_nfa_post_update_002_add_forest_purpose_terms(&$sandbox) {
-  $vocab = 'forest_purpose';
-  $forest_purpose_terms = [
-    'Timber' => [
-      'forest_type' => 'plantation',
-    ],
-    'Poles' => [
-      'forest_type' => 'plantation',
-    ],
-    'Fuel' => [
-      'forest_type' => 'plantation',
-    ],
-    'Processing' => [
-      'forest_type' => 'plantation',
-    ],
-    'Conservation' => [
-      'forest_type' => 'natural',
-    ],
-    'Restoration' => [
-      'forest_type' => 'natural',
-    ],
-  ];
-  foreach ($forest_purpose_terms as $term => $term_info) {
-    // Term names to be added.
-    $current_term = Term::create([
-      'name' => $term,
-      'vid' => $vocab,
-      'forest_type' => $term_info['forest_type'],
-    ]);
-    $current_term->save();
-  }
 }
