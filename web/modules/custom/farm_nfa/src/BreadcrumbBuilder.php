3c29f8cdb13db77da655823f10af5e87c2b9ab62
yash621 <yg17381@gmail.com>
2024-07-15 19:30:39 +0530

    #360: fixed subroute issues

--- web/modules/custom/farm_nfa/src/BreadcrumbBuilder.php
+++ web/modules/custom/farm_nfa/src/BreadcrumbBuilder.php
@@ -22,11 +22,10 @@ class BreadcrumbBuilder implements BreadcrumbBuilderInterface {
    * {@inheritdoc}
    */
   public function applies(RouteMatchInterface $route_match) {
-    $route = $route_match->getRouteObject();
     $asset = $route_match->getParameter('asset');
     $plan = $route_match->getParameter('plan');
     $view = $route_match->getParameter('view_id');
-    if ($asset instanceof EntityInterface || $plan instanceof PlanInterface || $view == 'farm_asset' || $view == 'farm_plan') {
+    if ($asset || $plan || $view == 'farm_asset' || $view == 'farm_plan') {
       return TRUE;
     }
     return FALSE;
@@ -36,6 +35,7 @@ class BreadcrumbBuilder implements BreadcrumbBuilderInterface {
    * {@inheritdoc}
    */
   public function build(RouteMatchInterface $route_match) {
+    error_log(print_r('testing 12345', TRUE));
     $breadcrumb = new Breadcrumb();
     $links = [];
     $asset = $route_match->getParameter('asset');
@@ -43,6 +43,7 @@ class BreadcrumbBuilder implements BreadcrumbBuilderInterface {
     $breadcrumb->addCacheContexts(['url.path']);
     $view = $route_match->getParameter('view_id');
     $links[] = Link::createFromRoute('Home', '<front>');
+    $current_tab = $this->getCurrentTab();
     if ($asset instanceof EntityInterface) {
       $asset_type = $asset->bundle();
       if ($asset_type == 'land') {
@@ -52,14 +53,26 @@ class BreadcrumbBuilder implements BreadcrumbBuilderInterface {
       $links[] = Link::createFromRoute('Locations', 'entity.asset.collection');
       $asset_type = $asset_type == 'cfr' ? strtoupper($asset_type) : ucfirst($asset_type);
       $links[] = Link::createFromRoute($asset_type, 'farm_nfa.assets.asset', ['asset' => strtolower($asset_type)]);
-      $links[] = Link::createFromRoute($asset->label(), '<none>');
+      if ($current_tab) {
+        $links[] = Link::createFromRoute($asset->label(), 'entity.asset.canonical', ['asset' => $asset->id()]);
+        $links[] = Link::createFromRoute(strtoupper($current_tab), '<none>');
+      }
+      else {
+        $links[] = Link::createFromRoute($asset->label(), '<none>');
+      }
     }
     elseif ($plan instanceof PlanInterface) {
       $plan_type = $plan->bundle();
       $plan_text = $plan_type == 'natural' ? 'Natural forest' : 'Plantation';
       $links[] = Link::createFromRoute('Plans', 'entity.plan.collection');
       $links[] = Link::createFromRoute($plan_text, 'farm_nfa.plans.plan', ['plan' => $plan_type]);
-      $links[] = Link::createFromRoute($plan->label(), '<none>');
+      if ($current_tab) {
+        $links[] = Link::createFromRoute($plan->label(), 'entity.plan.canonical', ['plan' => $plan->id()]);
+        $links[] = Link::createFromRoute(strtoupper($current_tab), '<none>');
+      }
+      else {
+        $links[] = Link::createFromRoute($plan->label(), '<none>');
+      }
     }
     elseif ($view == 'farm_asset') {
       $view_type = $route_match->getParameter('arg_0') ? $route_match->getParameter('arg_0') : $route_match->getParameter('display_id');
@@ -77,6 +90,20 @@ class BreadcrumbBuilder implements BreadcrumbBuilderInterface {
       $links[] = Link::createFromRoute($plan_text, '<none>');
     }
     return $breadcrumb->setLinks($links);
+  }
+
+  /**
+   * Helper function to get the current tab.
+   *
+   * @return string
+   *   The current tab.
+   */
+  private function getCurrentTab() {
+    $current_url = \Drupal::request()->getRequestUri();
+    $current_url = rtrim($current_url, '/');
+    $url_parts = explode('/', $current_url);
+    $last_part = end($url_parts);
+    return is_numeric($last_part) ? NULL : $last_part;
   }
 
 }
